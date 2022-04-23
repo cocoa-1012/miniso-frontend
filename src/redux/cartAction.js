bdfc7d6d0ee7c5d91ec54f7f64229ef1d842b7e8
Md Monirul Islam <mmislam027@gmail.com>
2022-04-23 22:13:56 +0600

    observations task 27  is done

--- src/redux/cartAction.js
+++ src/redux/cartAction.js
@@ -59,39 +59,43 @@ export const removeProductFromCart = (codInt, barra) => (dispatch) => {
     });
 };
 export const updateCartFromServer = (codInt, barra) => async (dispatch) => {
-  let username = localStorage.getItem('username');
-  let token = JSON.parse(localStorage.getItem('user'))?.access_token;
-  const api = `http://3.16.73.177:9080/private/cart/find?userName=${username}`;
-  //   let api = `/api/private/cart/find?userName=${username}`;
+  try {
+    let username = localStorage.getItem('username');
+    let token = JSON.parse(localStorage.getItem('user'))?.access_token;
+    const api = `http://3.16.73.177:9080/private/cart/find?userName=${username}`;
+    //   let api = `/api/private/cart/find?userName=${username}`;
 
-  axios
-    .get(api, {
-      widthCredentials: true,
-      crossdomain: true,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then(async (res) => {
-      const items = res.data.body.itemsDtos;
-      console.log(items);
-      const dataListPromise = items.map(
-        async ({ codeInt, codeBarrra, amount }) => {
-          const resData = await axios.get(
-            `http://3.16.73.177:9080/public/products/pk?codeInt=${codeInt}&barra=${codeBarrra}`
-          );
-          resData.amount = amount;
-          return resData;
-        }
-      );
-      const dataWithResponse = await Promise.all(dataListPromise);
-      console.log(dataWithResponse);
-      const allProducts = dataWithResponse.map((item) => {
-        item.data.body.amount = item.amount;
-        return item.data.body;
+    axios
+      .get(api, {
+        widthCredentials: true,
+        crossdomain: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(async (res) => {
+        const items = res?.data?.body?.itemsDtos;
+        console.log(items);
+        const dataListPromise = items.map(
+          async ({ codeInt, codeBarrra, amount }) => {
+            const resData = await axios.get(
+              `http://3.16.73.177:9080/public/products/pk?codeInt=${codeInt}&barra=${codeBarrra}`
+            );
+            resData.amount = amount;
+            return resData;
+          }
+        );
+        const dataWithResponse = await Promise.all(dataListPromise);
+        console.log(dataWithResponse);
+        const allProducts = dataWithResponse.map((item) => {
+          item.data.body.amount = item.amount;
+          return item.data.body;
+        });
+        dispatch(updateCart(allProducts));
       });
-      dispatch(updateCart(allProducts));
-    });
+  } catch (error) {
+    console.log('err-----', error);
+  }
 };
 export const clearCart = () => async (dispatch) => {
   dispatch(clear());
