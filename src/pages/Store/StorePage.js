aec0696b0c6dfdf04957fcc93122c77e7b689da1
Md Monirul Islam <mmislam027@gmail.com>
2022-04-25 23:55:15 +0600

    task 3 unique key props

--- src/pages/Store/StorePage.js
+++ src/pages/Store/StorePage.js
@@ -47,7 +47,10 @@ const StorePage = ({ noTitle = false }) => {
 
         <div className='row'>
           {stores.map((item) => (
-            <div className='col-12 col-sm-6 col-md-4 col-xxl-3 pb-4'>
+            <div
+              className='col-12 col-sm-6 col-md-4 col-xxl-3 pb-4'
+              key={Math.random()}
+            >
               <StoreCard {...item} />
             </div>
           ))}
